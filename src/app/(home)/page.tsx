21a024370c70e8b7b524a76bf790ca41c43a8b35
talisson <talissonb422@gmail.com>
2024-07-08 05:17:46 -0300

    feat: add second discount banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,13 +24,25 @@ export default async function Home() {
           quality={100}
           sizes="100vw"
           className="h-auto w-full"
-          alt="até 55% de desconto em mouses"
+          alt="até 55% de desconto só esse mês"
         />
       </div>
       <Categories />
 
-      <div className="pt-[30px]">
+      <div className="py-[30px]">
         <ProductList title="Ofertas" products={productsWithDiscount} />
+      </div>
+
+      <div className="px-5">
+        <Image
+          src="/banner-home-02.png"
+          width={0}
+          height={0}
+          quality={100}
+          sizes="100vw"
+          className="h-auto w-full"
+          alt="até 55% de desconto em mouses"
+        />
       </div>
     </>
   )
