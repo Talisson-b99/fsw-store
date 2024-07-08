869959ac9cc190f6a93e3adc4e75877d50768b02
talisson <talissonb422@gmail.com>
2024-07-08 07:09:42 -0300

    feat: add catalog page

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,30 @@
+import { LayoutGrid } from 'lucide-react'
+
+import { Badge } from '@/components/ui/badge'
+import prisma from '@/lib/prisma'
+
+import CatalogItem from './components/catalog-item'
+
+const Catalog = async () => {
+  const categories = await prisma.category.findMany({})
+
+  return (
+    <div className="px-5">
+      <Badge
+        variant={'outline'}
+        className="my-[30px] flex w-fit gap-2 px-3 py-2 font-semibold"
+      >
+        <LayoutGrid size={14} />
+        <span className="text-base">CATÁLAGO</span>
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8 pb-16">
+        {categories.map((categorie) => (
+          <CatalogItem key={categorie.id} category={categorie} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Catalog
