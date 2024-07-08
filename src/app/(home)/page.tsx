68709de12c4a30003ac2088715a4fc7ea654c276
talisson <talissonb422@gmail.com>
2024-07-08 02:28:27 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        width={0}
+        height={0}
+        quality={100}
+        sizes="100vw"
+        className="h-auto w-full"
+        alt="até 55% de desconto em mouses"
+      />
+    </div>
+  )
+}
