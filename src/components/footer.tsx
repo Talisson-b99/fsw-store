7bd12d8c8fe4de17866f798e62bc2cf391d5817b
talisson <talissonb422@gmail.com>
2024-07-16 13:38:33 -0300

    fix: error eslint footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-accent">
-      <p className="text-muted-foreground px-8 py-4 text-sm">
+      <p className="px-8 py-4 text-sm text-muted-foreground">
         &copy;{new Date().getFullYear()} Copyright{' '}
         <span className="font-semibold">Talisson Barbosa</span>
       </p>
