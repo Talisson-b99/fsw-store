74b22b54210c67e4828fc2a22f68c761e06a41fb
talisson <talissonb422@gmail.com>
2024-07-16 13:16:40 -0300

    feat: add stripe web hook

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -15,6 +15,7 @@ export const createCheckout = async (products: CartProduct[]) => {
     mode: 'payment',
     success_url: 'http://localhost:3000',
     cancel_url: 'http://localhost:3000',
+
     line_items: products.map((product: CartProduct) => {
       return {
         price_data: {
